d4d75f434639c96e82a5ccf19379d643e149dc91
chandu <chandrasr.palle@gmail.com>
2018-04-10 12:50:15 +0200

    player active class added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ activePlayer = 0;
 console.log(dice);
 
 document.querySelector("img").src="dice-"+6+".png";
+document.querySelector(".player-0-panel").className += " active"; 
+
 
 $(".btn-roll").click(function(){
     dice = Math.floor(Math.random()*6)+1;
@@ -20,25 +22,30 @@ $(".btn-roll").click(function(){
    else{
     document.querySelector("#current-"+activePlayer).innerHTML=0;
     roundScore=0;
+    document.querySelector(".player-"+ activePlayer +"-panel").className = "player-"+ activePlayer +"-panel";    
     if(activePlayer){
         activePlayer = 0;
+        document.querySelector(".player-0-panel").className += " active";
     }
     else{
         activePlayer = 1;
+        document.querySelector(".player-1-panel").className += " active"; 
     }
    }
 });
 $(".btn-hold").click(function(){
     scores[activePlayer] += roundScore; 
+    document.querySelector("#score-"+activePlayer).innerHTML = scores[activePlayer];
     roundScore = 0;
-    document.querySelector("#current-"+activePlayer).innerHTML=roundScore;
-    document.querySelector("#score-"+activePlayer).innerHTML = scores[activePlayer] ;
-
+    document.querySelector("#current-"+activePlayer).innerHTML = 0;
+    document.querySelector(".player-"+ activePlayer +"-panel").className = "player-"+ activePlayer +"-panel";
     if(activePlayer){
-        activePlayer = 0;       
+        activePlayer = 0;    
+        document.querySelector(".player-0-panel").className += " active";
     }
     else{
         activePlayer = 1;
+        document.querySelector(".player-1-panel").className += " active"; 
     }
 
 });
