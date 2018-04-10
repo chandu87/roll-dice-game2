d582181750b50a088e242e212d77d905df3db1bf
chandu <chandrasr.palle@gmail.com>
2018-04-10 16:27:43 +0200

    function created with repeated code

--- app.js
+++ app.js
@@ -16,37 +16,24 @@ $(".btn-roll").click(function(){
     dice = Math.floor(Math.random()*6)+1;
     document.querySelector("img").src="dice-"+dice+".png";
     if(dice !== 1){
-    roundScore+=dice;
-    document.querySelector("#current-"+activePlayer).innerHTML=roundScore;
+    roundScore += dice;
+    document.querySelector("#current-"+activePlayer).innerHTML =  roundScore;
    }
    else{
-    document.querySelector("#current-"+activePlayer).innerHTML=0;
-    roundScore=0;
-    document.querySelector(".player-"+ activePlayer +"-panel").className = "player-"+ activePlayer +"-panel";    
-    if(activePlayer){
-        activePlayer = 0;
-        document.querySelector(".player-0-panel").className += " active";
-    }
-    else{
-        activePlayer = 1;
-        document.querySelector(".player-1-panel").className += " active"; 
-    }
-   }
+    changePlayer();
+   } 
 });
 $(".btn-hold").click(function(){
     scores[activePlayer] += roundScore; 
     document.querySelector("#score-"+activePlayer).innerHTML = scores[activePlayer];
-    roundScore = 0;
-    document.querySelector("#current-"+activePlayer).innerHTML = 0;
-    document.querySelector(".player-"+ activePlayer +"-panel").className = "player-"+ activePlayer +"-panel";
-    if(activePlayer){
-        activePlayer = 0;    
-        document.querySelector(".player-0-panel").className += " active";
-    }
-    else{
-        activePlayer = 1;
-        document.querySelector(".player-1-panel").className += " active"; 
-    }
+    changePlayer();
 
 });
 
+function changePlayer(){
+    roundScore=0;
+    document.querySelector("#current-"+activePlayer).innerHTML=0;
+    document.querySelector(".player-"+ activePlayer +"-panel").classList.remove("active");    
+    activePlayer = activePlayer === 0 ? 1 : 0 ; 
+    document.querySelector(".player-"+ activePlayer + "-panel").classList.add("active");
+}
