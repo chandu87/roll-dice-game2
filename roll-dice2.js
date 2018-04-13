2944dfd1c1c9098fc5b53c74d62d857acbc01477
chandu <chandrasr.palle@gmail.com>
2018-04-13 11:08:02 +0200

    initial commit from version1

--- roll-dice2.js
+++ roll-dice2.js
@@ -1,34 +1,38 @@
 
-var scores, roundScore, activePlayer, dice, prevDice;
-prevDice = 0;
+var scores, roundScore, activePlayer, dice1, dice2;
 var gameScore;
 
 initGame();
 
 document.querySelector(".btn-roll").addEventListener('click',function(){
     if(scores[activePlayer] < gameScore){
-        dice = Math.floor(Math.random()*6)+1;
-        document.querySelector("img").style.display = 'block';
-        document.querySelector("img").src = "images/dice-" + dice + ".png";
-        if(dice ===6 && prevDice === 6){
+        dice1 = Math.floor(Math.random()*6)+1;
+        dice2 = Math.floor(Math.random()*6)+1;
+        document.querySelector(".dice").style.display = 'block';
+        document.querySelector(".dice2").style.display = 'block';
+        document.querySelector(".dice").src = "images/dice-" + dice1 + ".png";
+        document.querySelector(".dice2").src = "images/dice-" + dice2 + ".png";
+
+        if(dice1 ===6 && dice2 === 6){
             scores[activePlayer] = 0;
             document.querySelector("#score-"+activePlayer).innerHTML =  0;
             changePlayer();
         }
-        else if(dice !== 1){
-        roundScore += dice;
+        else if((dice1 !== 1) && (dice2 !==1)){
+        roundScore += (dice1 + dice2);
         document.querySelector("#current-"+activePlayer).innerHTML =  roundScore;
         }
         else{
         changePlayer();
         }
 }
-prevDice = dice; 
+ 
 });
 
 document.querySelector(".btn-hold").addEventListener('click',function(){
     if(scores[activePlayer] < gameScore){
-        document.querySelector("img").style.display = 'none';
+        document.querySelector(".dice").style.display = 'none';
+        document.querySelector(".dice2").style.display = 'none';
         scores[activePlayer] += roundScore; 
         document.querySelector("#score-"+activePlayer).innerHTML = scores[activePlayer];
         
@@ -67,7 +71,8 @@ function initGame(){
     gameScore = 100;    
     
     // not displaying dice when you open game
-    document.querySelector("img").style.display = 'none';
+    document.querySelector(".dice").style.display = 'none';
+    document.querySelector(".dice2").style.display = 'none';
     
     document.querySelector("#score-0").innerHTML = 0;
     document.querySelector("#score-1").innerHTML = 0;
